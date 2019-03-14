bb837d20d9caf51aba3347a7df8b9b1a44f238a0
Courtney Gibson <courtney.gibson@centre.edu>
2019-03-14 19:22:19 -0400

    edit

--- dayData.js
+++ dayData.js
@@ -4,18 +4,29 @@ gradeDataP.then(function(data)
 {
   var width = 600;
   var height = 400;
-  var current = 0;
+  var barWidth = width/data[0].grades.length;
+
+  var yScale = d3.scaleLinear()
+                .domain([0,100])
+                .range([height,0]);
+
+  var xScale = d3.scaleLinear()
+                .domain([0, barWidth*4+40])
+                .range([0, width]);
+
+
   console.log("data", data);
-  init(data, width, height, 0);
-  var currDay = update(data, current);
-  
+  init(data, width, height, 0, xScale, yScale);
+  update(data, width, height, xScale, yScale);
+
+
 },
   function(err)
 {
   console.log(err);
 })
 
-var init = function(data, w, h, current)
+var init = function(data, w, h, current, xScale, yScale)
 {
   var barWidth = w/data[0].grades.length;
   var svg = d3.select("svg")
@@ -26,19 +37,59 @@ var init = function(data, w, h, current)
       .data(data[0].grades)
       .enter()
       .append("rect")
-      .attr("height", function(d){return 200;})
+      .attr("height", function(d){return 3;})
       .attr("width", function(d){return barWidth;})
-      .attr("x", function(d, i){return (i*barWidth)+(i*10);})
-      .attr("y", function(d, i){return 0;})
+      .attr("x", function(d, i){return xScale(i+barWidth*i+20*i);})
+      .attr("y", function(d, i){return h-yScale(100-d.grades);})
       .attr("fill","blue");
 }
 
-var update = function(changeVal, data, current)
+var update = function(data, w, h, xScale, yScale)
+{
+  var barWidth = w/data[0].grades.length;
+  var svg = d3.select("svg");
+  var prev = d3.select("#prev")
+  var next = d3.select("#next")
+
+  prev.on("click", function(d, i)
+{
+
+  var currDay = parseInt(document.getElementById("curr").textContent);
+  console.log("prev button press: index ", currDay-2)
+  var newDay = currDay - 1;
+  if (newDay > 0 && newDay <= 10)
+  {
+    var newCurrDay = document.getElementById("curr").innerHTML = newDay;
+
+    svg.selectAll("rect")
+        .data(data[currDay-2].grades)
+        .attr("height", function(d,i){return d.grade;})
+        .attr("width", function(d){return barWidth;})
+        .attr("x", function(d, i){return xScale(i+barWidth*i+20*i);})
+        .attr("y", function(d, i){return h-yScale(100-d.grades);})
+  }
+
+});
+
+  next.on("click", function(d, i)
 {
   var currDay = parseInt(document.getElementById("curr").textContent);
-  currDay += changeVal;
-  var newCurrDay = document.getElementById("curr").innerHTML = currDay;
+  console.log("next button press: index ", currDay)
+  var newDay = currDay + 1;
+  console.log("data.grades.grade", data[0].grades[0].grade)
+  if (newDay >= 0 && newDay <= 10)
+  {
+    var newCurrDay = document.getElementById("curr").innerHTML = newDay;
 
-  return currDay;
+
+    svg.selectAll("rect")
+        .data(data[currDay].grades)
+        .attr("height", function(d,i){return d.grade;})
+        .attr("width", function(d){return barWidth;})
+        .attr("x", function(d, i){return xScale(i+barWidth*i+20*i);})
+        .attr("y", function(d, i){return h-yScale(100-d.grades);})
+  }
+
+});
 
 }
