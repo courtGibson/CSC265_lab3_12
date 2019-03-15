94953bc4c0b4aa6c7c3e39519eeba40fd6b47713
Courtney Gibson <courtney.gibson@centre.edu>
2019-03-14 20:22:44 -0400

    edit

--- dayData.js
+++ dayData.js
@@ -4,20 +4,24 @@ gradeDataP.then(function(data)
 {
   var width = 600;
   var height = 400;
-  var barWidth = width/data[0].grades.length;
+  var barWidth = width/4;
 
   var yScale = d3.scaleLinear()
                 .domain([0,100])
-                .range([height,0]);
+                .range([400, 0]);
 
   var xScale = d3.scaleLinear()
-                .domain([0, barWidth*4+40])
-                .range([0, width]);
+                .domain([0,4])
+                .range([0,width])
 
+
+  /*d3.scaleOrdinal()
+                .domain([1,2,3,4])
+                .range([0,width]);*/
 
   console.log("data", data);
-  init(data, width, height, 0, xScale, yScale);
-  update(data, width, height, xScale, yScale);
+  init(data, width, height, 0, xScale, yScale, barWidth);
+  update(data, width, height, xScale, yScale, barWidth);
 
 
 },
@@ -26,9 +30,9 @@ gradeDataP.then(function(data)
   console.log(err);
 })
 
-var init = function(data, w, h, current, xScale, yScale)
+var init = function(data, w, h, current, xScale, yScale, barWidth)
 {
-  var barWidth = w/data[0].grades.length;
+
   var svg = d3.select("svg")
               .attr("width", w)
               .attr("height", h)
@@ -37,16 +41,15 @@ var init = function(data, w, h, current, xScale, yScale)
       .data(data[0].grades)
       .enter()
       .append("rect")
-      .attr("height", function(d){return 3;})
-      .attr("width", function(d){return barWidth;})
-      .attr("x", function(d, i){return xScale(i+barWidth*i+20*i);})
-      .attr("y", function(d, i){return h-yScale(100-d.grades);})
+      .attr("height", function(d){return yScale(3);})
+      .attr("width", function(d){return xScale(barWidth);})
+      .attr("x", function(d, i){return xScale(d.name);})
+      .attr("y", function(d, i){return h-yScale(parseInt(d.grade));})
       .attr("fill","blue");
 }
 
-var update = function(data, w, h, xScale, yScale)
+var update = function(data, w, h, xScale, yScale, barWidth)
 {
-  var barWidth = w/data[0].grades.length;
   var svg = d3.select("svg");
   var prev = d3.select("#prev")
   var next = d3.select("#next")
@@ -63,10 +66,10 @@ var update = function(data, w, h, xScale, yScale)
 
     svg.selectAll("rect")
         .data(data[currDay-2].grades)
-        .attr("height", function(d,i){return d.grade;})
+        .attr("height", function(d,i){return yScale(d.grade);})
         .attr("width", function(d){return barWidth;})
-        .attr("x", function(d, i){return xScale(i+barWidth*i+20*i);})
-        .attr("y", function(d, i){return h-yScale(100-d.grades);})
+        .attr("x", function(d, i){return xScale(i);})
+        .attr("y", function(d, i){return h-yScale(parseInt(d.grade));})
   }
 
 });
@@ -84,10 +87,10 @@ var update = function(data, w, h, xScale, yScale)
 
     svg.selectAll("rect")
         .data(data[currDay].grades)
-        .attr("height", function(d,i){return d.grade;})
+        .attr("height", function(d,i){return yScale(d.grade);})
         .attr("width", function(d){return barWidth;})
-        .attr("x", function(d, i){return xScale(i+barWidth*i+20*i);})
-        .attr("y", function(d, i){return h-yScale(100-d.grades);})
+        .attr("x", function(d, i){return xScale(i);})
+        .attr("y", function(d, i){return h-yScale(parseInt(d.grade));})
   }
 
 });
